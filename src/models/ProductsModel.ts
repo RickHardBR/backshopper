f6bd5c2847a151f9ec5cda3cf9802d24e349fd97
RickHardBR <rickhard@bol.com.br>
2022-10-25 23:10:18 -0300

    Edit arquive

--- src/models/ProductsModel.ts
+++ src/models/ProductsModel.ts
@@ -1,6 +1,6 @@
 import { IProductTypeModel } from './ProductTypeModel';
 
 export interface IProductsModel {
-  getAllProducts:(page: number, nameProduct?: string, id?: number, selectQty?:number)=> Promise<IProductTypeModel[]>;
+  getAllProducts:(page: number, nameProduct?: string)=> Promise<IProductTypeModel[]>;
   
 }
